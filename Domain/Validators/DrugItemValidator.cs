2307ddf1493a0e68f9c9789cbf7fae2849797ea0
sazonov2703 <danad270306@gmail.com>
2024-12-06 18:37:52 +0200

    Revert "Homework"
    This reverts commit 1e75bf515206d7d4d5b76601d42e0dcbc3df0f3d.

--- Domain/Validators/DrugItemValidator.cs
+++ Domain/Validators/DrugItemValidator.cs
@@ -8,26 +8,17 @@ using FluentValidation;
 using Domain.Entities;
 using Domain.Primitives;
 
-/// <sumary>
-/// Валидатор полей для создания связи между препаратом и аптекой.
-/// </sumary>
 namespace Domain.Validators
 {
     public class DrugItemValidator : AbstractValidator<DrugItem>
     {
         public DrugItemValidator()
         {
-            /// <sumary>
-            /// Правила для поля Цена. Больше нуля, меньше 9999999, должна содержать не более двух знаков после запятой.
-            /// </sumary>
             RuleFor(di => di.Cost)
                 .GreaterThan(0).WithMessage(ValidationMessage.InvalidValueMessage)
                 .LessThan(9999999).WithMessage(ValidationMessage.InvalidValueMessage)
                 .Must(cost => cost == Math.Round(cost, 2)).WithMessage(ValidationMessage.InvalidValueMessage);
 
-            /// <sumary>
-            /// Правила для поля Количество. Больше нуля, меньше 10000.
-            /// </sumary>
             RuleFor(di => di.Count)
                 .GreaterThanOrEqualTo(0).WithMessage(ValidationMessage.InvalidValueMessage)
                 .LessThanOrEqualTo(10000).WithMessage(ValidationMessage.InvalidValueMessage);
--- Domain/Validators/DrugStoreValidator.cs
+++ Domain/Validators/DrugStoreValidator.cs
@@ -5,26 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
 using Domain.Entities;
+
 using Domain.Primitives;
 
-/// <sumary>
-/// Валидатор полей для создания аптеки.
-/// </sumary>
 namespace Domain.Validators
 {
     public class DrugStoreValidator : AbstractValidator<DrugStore>
     {
         public DrugStoreValidator()
         {
-            /// <sumary>
-            /// Правило для поля Аптечная сеть. Длина от 2 до 100 символов.
-            /// </sumary>
             RuleFor(ds => ds.DrugNetwork)
                 .Length(2, 100).WithMessage(ValidationMessage.LengthMessage);
-
-            /// <sumary>
-            /// Правило для поля Номер. Должен быть больше нуля, либо равен нулю.
-            /// </sumary>
             RuleFor(ds => ds.Number)
                 .GreaterThanOrEqualTo(0).WithMessage(ValidationMessage.InvalidValueMessage);
         }
